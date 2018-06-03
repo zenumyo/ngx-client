441653e7183154c93157bfd045aacef7a15e6747
shakil675 <shakil.ansari589@gmail.com>
2018-06-03 22:51:45 +0530

    update logs link

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -6,8 +6,10 @@ import { Component } from '@angular/core';
   template: `
     <span class="created-by">Copyright@ <b><a href="#" target="_blank">Yome</a></b> 2018</span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="https://www.facebook.com/Yomefoods/" target="_blank" class="ion ion-social-facebook"></a>
+      <a href="https://twitter.com/yomefoods" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="https://www.youtube.com/channel/UCXHpRB4GoRqVUJA4ssdU7QQ/featured?view_as=subscriber" target="_blank" class="ion ion-social-youtube"></a>
+      <a href="https://plus.google.com/u/1/107014639896263346830" target="_blank" class="ion ion-social-googleplus"></a>
       <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
   `,
